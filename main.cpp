de340643fa3a3231ac71d8f0eaafb1238d316a9a
Ian Liu Rodrigues <ian.liu88@gmail.com>
2020-09-22 13:46:02 -0300

    Improve test layout; change row order

--- main.cpp
+++ main.cpp
@@ -43,19 +43,17 @@ typedef uint8_t u8;
  */
 
 /**
- * Layout is topologically equivalent to a 10x6 matrix:
- *
  * ┌                                           ┐
- * │                      (D R5) (E R5) (F R5) │
- * │ (A R4) (B R4) (C R4) (D R4) (E R4) (F R4) │
- * │ (A R3) (B R3) (C R3) (D R3) (E R3) (F R3) │
- * │ (A R2) (B R2) (C R2) (D R2) (E R2) (F R2) │
- * │ (A R1) (B R1) (C R1) (D R1) (E R1) (F R1) │
  * │ (A L1) (B L1) (C L1) (D L1) (E L1) (F L1) │
  * │ (A L2) (B L2) (C L2) (D L2) (E L2) (F L2) │
  * │ (A L3) (B L3) (C L3) (D L3) (E L3) (F L3) │
  * │ (A L4) (B L4) (C L4) (D L4) (E L4) (F L4) │
  * │                      (D L5) (E L5) (F L5) │
+ * │ (A R1) (B R1) (C R1) (D R1) (E R1) (F R1) │
+ * │ (A R2) (B R2) (C R2) (D R2) (E R2) (F R2) │
+ * │ (A R3) (B R3) (C R3) (D R3) (E R3) (F R3) │
+ * │ (A R4) (B R4) (C R4) (D R4) (E R4) (F R4) │
+ * │                      (D R5) (E R5) (F R5) │
  * └                                           ┘
  */
 
@@ -88,29 +86,30 @@ const u8 cols[] = {
 };
 
 const u8 rows[] = {
-        10, // R5
-        16, // R4
-        14, // R3
-        15, // R2
-        18, // R1
 	5,  // L1
 	6,  // L2
 	7,  // L3
 	8,  // L4
 	9,  // L5
+	18, // R1
+	15, // R2
+	14, // R3
+	16, // R4
+	10, // R5
 };
 
 static const u8 layout0[] = {
-	'a', 'b', 'c', 'd', 'e', 'f',
-	'g', 'h', 'i', 'j', 'k', 'l',
-	'm', 'n', 'o', 'p', 'q', 'r',
-	's', 't', 'u', 'v', 'w', 'x',
-	'y', 'z', '0', '1', '2', '3',
-	'4', '5', '6', '7', '8', '9',
-	'~', '!', '@', '#', '$', '%',
-	'^', '&', '*', '(', ')', '-',
-	'+', '[', ']', '{', '}', '/',
-	',', ';', '.', '"', '`', '=',
+	KEY_ESC, '1', '2', '3', '4', '5',
+	KEY_TAB, 'q', 'w', 'e', 'r', 't',
+	KEY_LEFT_CTRL, 'a', 's', 'd', 'f', 'g',
+	KEY_LEFT_SHIFT, 'z', 'x', 'c', 'v', 'b',
+	'\0', '\0', '\0', KEY_LEFT_ALT, KEY_LEFT_GUI, ' ',
+
+	KEY_BACKSPACE, '0', '9', '8', '7', '6',
+	'\\', 'p', 'o', 'i', 'u', 'y',
+	'\'', ';', 'l', 'k', 'j', 'h',
+	KEY_RETURN, '/', '.', ',', 'm', 'n',
+	'\0', '\0', '\0', ']', '[', ' ',
 };
 
 const u8 ncols = sizeof(cols) / sizeof(cols[0]);
